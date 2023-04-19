d3614ea1624a14c923f456bf28ac40ddfb06da23
Sukaniya <131241928+Sukaniya@users.noreply.github.com>
2023-04-19 14:29:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.markdown("<h1 style='text-align: center; color: red;'> Application to find the largest of 3 numbers</h1>",unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: green;'> Application to find the largest of 3 numbers</h1>",unsafe_allow_html=True)
 
 num1 = st.number_input("Enter the first number")
 num2 = st.number_input("Enter the second number")
